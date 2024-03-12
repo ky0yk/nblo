a5ec06d08de2c177a02129f75e6b3573c5c5549e
ky0yk <40429527+ky0yk@users.noreply.github.com>
2024-03-12 21:58:36 +0900

    listを仮実装

---
+++ packages/server/src/use-case/list-articles.ts
@@ -0,0 +1,45 @@
+import { AuthorId, SavedArticle } from '../domain/model/article';
+import {
+  fetchArticlesByAuthor,
+  fetchOwnArticles,
+  fetchArticles,
+} from '../infra/repository/article/fetch-articles';
+
+export type QueryParams = {
+  authorId?: AuthorId;
+  sort?: 'asc' | 'desc';
+  startDate?: string;
+  endDate?: string;
+  token?: string;
+};
+
+interface ArticleSummary {
+  articleId: string;
+  authorId: string;
+  title: string;
+  createdAt: string;
+}
+
+export const listArticles = (params: QueryParams) => {
+  // トークンがあるとき
+  if (params.token) {
+    return fetchOwnArticles(params).map(toSummary);
+  }
+  // 著者IDが指定されているとき
+  else if (params.authorId) {
+    return fetchArticlesByAuthor(params).map(toSummary);
+  }
+  // どの条件にも当てはまらないとき
+  else {
+    return fetchArticles(params).map(toSummary);
+  }
+};
+
+const toSummary = (article: SavedArticle): ArticleSummary => {
+  return {
+    articleId: article.articleId,
+    authorId: article.authorId,
+    title: article.title,
+    createdAt: article.createdAt,
+  };
+};
