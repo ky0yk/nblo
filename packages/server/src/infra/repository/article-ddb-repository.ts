e79364b116c5d832333962a8864f0fad1a8e2eae
ky0yk <40429527+ky0yk@users.noreply.github.com>
2024-04-18 13:40:25 +0900

    インフラ関数を1ファイルにまとめた

---
+++ packages/server/src/infra/repository/article-ddb-repository.ts
@@ -0,0 +1,102 @@
+
+import { DeleteCommandInput } from '@aws-sdk/lib-dynamodb';
+import { v4 as uuidv4 } from 'uuid';
+import { PutCommandInput } from '@aws-sdk/lib-dynamodb';
+import { DeleteArticleById, FindArticleById, SaveArticle } from '@/domain/article/interface/article-repository';
+import { QueryCommandInput } from '@aws-sdk/lib-dynamodb';
+import { unmarshall } from '@aws-sdk/util-dynamodb';
+import { DynamoDbResultClient } from '../shared/dynamodb-result-client';
+import { SavedArticle } from '@/domain/article/model/article';
+
+
+
+export const makeDeleteArticleById = (
+  ddbResultClient: DynamoDbResultClient,
+) => {
+  const deleteArticleById: DeleteArticleById = ({ authorId, articleId }) => {
+    const TABLE_NAME = process.env.TABLE_NAME;
+
+    const input: DeleteCommandInput = {
+      TableName: TABLE_NAME,
+      Key: {
+        authorId: authorId,
+        articleId: articleId,
+      },
+    };
+
+    return ddbResultClient
+      .deleteItem(input)
+      .map(() => {})
+      .mapErr(
+        (error) => new Error(`Failed to delete the article: ${error.message}`),
+      );
+  };
+
+  return deleteArticleById;
+};
+
+
+export const makeFindArticleById = (client: DynamoDbResultClient) => {
+  const findArticleById: FindArticleById = (articleId) => {
+    const TABLE_NAME = process.env.TABLE_NAME;
+    const GSI_NAME = 'ArticleIdIndex';
+
+    const MAX_QUERY_LIMIT = 10;
+
+    const input: QueryCommandInput = {
+      TableName: TABLE_NAME,
+      IndexName: GSI_NAME,
+      KeyConditionExpression: 'articleId = :articleId',
+      ExpressionAttributeValues: {
+        ':articleId': { S: articleId },
+      },
+    };
+
+    return client
+      .queryRecursively(input, MAX_QUERY_LIMIT)
+      .map((queryOutput) => {
+        if (!queryOutput.Items || queryOutput.Items.length === 0) {
+          throw new Error('Article not found');
+        }
+        return unmarshall(queryOutput.Items[0]) as SavedArticle;
+      })
+      .mapErr(
+        (error) =>
+          new Error(
+            `Failed to get the article: ${error.message || error.toString()}`,
+          ),
+      );
+  };
+
+  return findArticleById;
+};
+
+
+export const makeSaveArticle = (ddbResultClient: DynamoDbResultClient) => {
+  const saveArticle: SaveArticle = (model) => {
+    const TABLE_NAME = process.env.TABLE_NAME || 'Articles';
+
+    const articleId = model.articleId ? model.articleId : uuidv4();
+    const article = {
+      ...model,
+      articleId,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    };
+
+    const input: PutCommandInput = {
+      TableName: TABLE_NAME,
+      Item: article,
+    };
+
+    return ddbResultClient
+      .putItem(input)
+      .map(() => article)
+      .mapErr(
+        (error) =>
+          new Error('Failed to save the blog article: ' + error.message),
+      );
+  };
+
+  return saveArticle;
+};
